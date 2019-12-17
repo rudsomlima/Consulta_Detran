55e49f2f1b6f26984eabb38db4a92223453fb232
rudsom.lima <smolder>
2019-12-17 11:52:41 -0300

    Não funcionando

--- content.js
+++ content.js
@@ -3,12 +3,11 @@ chrome.runtime.onMessage.addListener(
         if( request.message === "clicked_browser_action" ) {
             // var placa = document.querySelector("* > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(6) > div:nth-child(2) > dl:nth-child(1) > dd ").textContent;
             // var renavam = document.querySelector("* > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(6) > div:nth-child(2) > dl:nth-child(5) > dd").textContent;
-            alert("Entrou em content.js");
-            var placa =  "NNN9393";
-            var renavam = "989873196";
-            console.log(placa);
-            console.log(renavam);
-            window.open("http://www2.detran.rn.gov.br/externo/consultarveiculo.asp", "_blank");
+            // var placa =  "NNN9393";
+            // var renavam = "989873196";
+            // console.log(placa);
+            // console.log(renavam);
+            chrome.runtime.sendMessage({"message": "consulta_back"});
         }
     }
 );

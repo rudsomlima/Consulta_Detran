1c2a27e21657125d072efcee04f3f3d269e6df59
rudsom.lima <smolder>
2019-12-17 14:28:31 -0300

    Funcionando OK

--- consulta.js
+++ consulta.js
@@ -1,8 +1,14 @@
-alert("Entrou na consulta.js");
-var placa =  "NNN9393";
-var renavam = "989873196";
-console.log(placa);
-console.log(renavam);
-$("#placa").val(placa);
-$("#renavam").val(renavam);
-$("#btnConsultaPlaca").click();
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.message === "consulta" ) {
+            var placa = request.placa;
+            var renavam = request.renavam;
+            alert(placa + "\n" + renavam);
+            console.log(placa);
+            console.log(renavam);
+            $("#placa").val(placa);
+            $("#renavam").val(renavam);
+            $("#btnConsultaPlaca").click();
+        }
+    }
+);
--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if( request.message === "clicked_browser_action" ) {
+
             // var placa = document.querySelector("* > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(6) > div:nth-child(2) > dl:nth-child(1) > dd ").textContent;
             // var renavam = document.querySelector("* > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(6) > div:nth-child(2) > dl:nth-child(5) > dd").textContent;
             // var placa =  "NNN9393";
